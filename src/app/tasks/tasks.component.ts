a095cdf57c6003d0e5385c3e0d6041ced67e0b85
unknown <harsh710patel@gmail.com>
2023-08-08 18:10:16 +0530

    completing modal

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
+import { Component, Input } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent {
+  inSubmission = false;
+  title = new FormControl<string>('', [
+    Validators.required,
+    Validators.minLength(3),
+  ]);
 
+  description = new FormControl<string>('', [
+    Validators.required,
+    Validators.minLength(3),
+  ]);
+
+  details = new FormControl<string>('', [
+    Validators.required,
+    Validators.minLength(3),
+  ]);
+
+  company = new FormControl<string>('', [
+    Validators.required,
+    Validators.minLength(3),
+  ]);
+
+  status = new FormControl<string>('', [
+    Validators.required,
+    Validators.minLength(3),
+  ]);
+
+  priority = new FormControl<string>('', [
+    Validators.required,
+    Validators.minLength(3),
+  ]);
+
+  duration = new FormControl<string>('', [
+    Validators.required,
+    Validators.minLength(3),
+  ]);
+
+  taskForm = new FormGroup({
+    title: this.title, //
+    description: this.description, //
+    details: this.details, //
+    company: this.company, //
+    status: this.status, //
+    priority: this.priority, //
+    duration: this.duration, //
+  });
+
+  submit() {
+    console.log(this.taskForm.value);
+    // this.auth.register(this.taskForm.value);
+  }
+  constructor(protected modalService: ModalService) {}
 }
